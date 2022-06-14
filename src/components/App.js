9e57ecfd93c8c4646a69ff69ddacc73141ed3908
= <f7lipe@me.com>
2022-06-14 15:55:22 -0300

    feat: signup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {useState, useContext} from "react";
 import React from 'react';
+import Signup from './Signup';
+import UserContext from "../contexts/UserContext";
 
 export default function App(){
+    const [token, setToken] = useState(localStorage.getItem('likr-user-token'))
     return(
-        <>
-        </>
+        <UserContext.Provider value={{ token, setToken }}>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/signup" element={<Signup/>} />
+        </Routes>
+    </BrowserRouter>
+    </UserContext.Provider>
     );
 }
