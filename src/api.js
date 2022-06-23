4fcaedb3be1b37b8cf1d6379e426dbe07cf4648e
Luis Felipe Vanin <luisfvaninmartins@gmail.com>
2022-06-23 00:14:16 -0300

    first commit from the branch

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://driven-linkr.herokuapp.com/",
+    // baseURL: "https://driven-linkr.herokuapp.com/",
+    baseURL: "http://localhost:5000/",
 });
 
 export default api;
